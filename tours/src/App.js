b7ca96bf3e591e224da1f8e929b0b7ed8af839ac
Atanu Paul <atanupaul017@gmail.com>
2021-05-16 11:53:03 +0530

    modify app component

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loding from "./Components/Loding";
+import Tours from "./Components/Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loding, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loding) {
+    return (
+      <div>
+        <Loding />
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <h1>tours</h1>
+    <div>
+      <Tours tours={tours} />
     </div>
   );
 }
